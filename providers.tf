6453754cd07235d8e119a4051eb40ef779ae46f4
Geert van der Cruijsen <geert@vdcruijsen.net>
2025-12-30 14:22:49 +0100

    provider fix

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 # Provider configurations
+# Note: When using Terranetes, the Azure provider is automatically configured
+# by the controller based on the providerRef in cloudresource.yaml
+# Do not add a provider "azurerm" block here as it will cause conflicts
 
-# Configure the Azure Provider
-provider "azurerm" {
-  features {}
-}
-
-# Configure the GitHub Provider
+# Configure the GitHub Provider (still needed for GitHub resources)
 provider "github" {
   # GitHub token should be provided via GITHUB_TOKEN environment variable
   # or through GitHub CLI authentication
--- versions.tf
+++ versions.tf
@@ -1,11 +1,12 @@
 # OpenTofu/Terraform version and provider requirements
 # Note: OpenTofu uses the same "terraform" block syntax for compatibility
+# When using Terranetes, the backend is automatically configured to use Kubernetes
+# Do not add a backend block here as it will cause conflicts
 
 terraform {
   required_version = ">= 1.0"
   
-  backend "azurerm" {
-  }
+  # Backend configuration removed - Terranetes uses Kubernetes backend
   
   required_providers {
     azurerm = {
